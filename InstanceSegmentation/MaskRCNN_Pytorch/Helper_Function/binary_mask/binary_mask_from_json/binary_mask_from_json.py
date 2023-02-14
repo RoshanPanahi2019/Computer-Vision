9b6667d722dacc740168545671f85eadc3a9889f
Roshan <roshanpanahico@gmail.com>
2023-02-13 18:50:52 -0800

    Works for single class.

--- InstanceSegmentation/MaskRCNN_Pytorch/Helper_Function/binary_mask/binary_mask_from_json/binary_mask_from_json.py
+++ InstanceSegmentation/MaskRCNN_Pytorch/Helper_Function/binary_mask/binary_mask_from_json/binary_mask_from_json.py
@@ -43,17 +43,22 @@ def single_class_mask():
 def multi_class_mask():
     path="/media/mst/Backup/dataset/InstanceSegmentation/myDataset_Augmentation/Annotation/18/via_project_13Feb2023_12h25m_json.json"
     with open(path, "r") as my_json:
-        data=json.load(my_json)
-    for name in data.keys():
-        image_name=data[name]['filename']
+        json_per_image=json.load(my_json)
+    for key in json_per_image.keys():
+        image_name=json_per_image[key]['filename'] 
         img = np.asarray(PIL.Image.open('/media/mst/Backup/dataset/InstanceSegmentation/myDataset_Augmentation/Images/'+image_name))    
-        mask = np.zeros((img.shape[0],img.shape[1]))
-        print(mask.shape)
-        # Continue to create the mask 
+        mask = np.zeros((img.shape[0],img.shape[1])) # draw a blank canvas
+        for i in range (len(json_per_image[key]["regions"])):
+            shape1_x=json_per_image[key]["regions"][i]['shape_attributes']['all_points_x']
+            shape1_y=json_per_image[key]["regions"][i]['shape_attributes']['all_points_y']
+            lbl=label[json_per_image[key]["regions"][i]["region_attributes"]["Component"]] # Return the label for each region
+            ab=np.stack((shape1_x, shape1_y), axis=1)
+            img3=cv2.drawContours(mask, [ab], -1, i+1, -1) # read the loader in Mask R-CNN and change the colors accordingly.
+        cv2.imwrite('/media/mst/Backup/dataset/InstanceSegmentation/myDataset_Augmentation/Mask/' +image_name[:-5]+'.png',mask.astype(np.uint8))
 
-    
-
-
+#==================================
 if __name__=="__main__":
     #single_class_mask()
+    label={"Module":0,"BathPod":1}
+    color={}
     multi_class_mask()
